dbba2794b5017e34650d5162ec718160b0609b35
George Thomas <georgesilverbeech@gmail.com>
2023-12-30 22:12:54 +0000

    Added removeFirst/Last

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -5,24 +5,26 @@ public class LinkedList {
     private Node tail;
     private int size;
 
-    private boolean isEmpty(){
+    private boolean isEmpty() {
         return size == 0;
     }
+
     private void addFirst(int data) {
         Node newNode = new Node(data);
-        if (isEmpty()){
+        if (isEmpty()) {
             head = newNode;
         }
         newNode.next = head;
         head.prev = newNode;
         head = newNode;
-        if (tail == null){
+        if (tail == null) {
             tail = newNode;
         }
+        size++;
     }
 
-    private void addLast(int data){
-        if (isEmpty()){
+    private void addLast(int data) {
+        if (isEmpty()) {
             addFirst(data);
         }
         Node newNode = new Node(data);
@@ -32,12 +34,32 @@ public class LinkedList {
         size++;
     }
 
-    private int removeFirst(){
-        return 1;
+    private int removeFirst() {
+        if (isEmpty()) {
+            throw new RuntimeException("Cannot remove an item from an empty list");
+        }
+        int headData = head.data;
+        head.next.prev = null;
+        head = head.next;
+        size--;
+        return headData;
     }
 
-    private int removeLast(){
-        return 1;
-    }
+    private int removeLast() {
+        if (isEmpty()) {
+            throw new RuntimeException("Cannot remove an item from an empty list");
+        }
+        int tailData = tail.data;
+        tail = tail.prev;
+        if (tail != null){
+            tail.next = null;
+        }
+        else {
+            head = null;
+        }
+        size--;
+        return tailData;
 
+
+    }
 }
